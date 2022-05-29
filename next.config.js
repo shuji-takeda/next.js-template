bd76498824b5d14ed8eaa9b92e9e3f3bd972c65f
shuji-takeda <12.hack.17.takeshu@gmail.com>
2022-05-29 15:52:30 +0900

    tsconfig&next.config modify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    customKey: '#####',
+  },
+  pageExtensions: ['mdx', 'md', 'jsx', 'tsx', 'ts'],
+};
 
 module.exports = nextConfig
